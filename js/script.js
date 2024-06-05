c7d4bc1a7cb416eed9bd2b9440b578692cc8fc4d
lazzaric92 <lazzaric_92@hotmail.it>
2024-06-05 19:10:02 +0200

    added function to generate a given number of emails

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            emailArray: []
         }
-    } 
+    },
+    methods: {
+        generateRandomEmail: function(numberOfEmails){
+            let index = 0
+            while(index < numberOfEmails){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    email = response.data.response;
+                    this.emailArray.push(email);
+                });
+                index++;
+            }
+            console.log(this.emailArray);
+        }
+    },
+    created(){
+        this.generateRandomEmail(10);
+    }
 }).mount('#app');
